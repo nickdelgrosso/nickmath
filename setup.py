571416cc1fcf3117e22b4260ea8f061a5f838f55
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2018-08-16 14:14:44 +0200

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='coolpy',
+    version='0.1',
+    packages=['nickmath'],
+    url='www.mysite.com',
+    license='MIT',
+    author='nickdg',
+    author_email='delgrosso.nick@gmail.com',
+    description='The worst math operation package ever.  Do not use!'
+)
