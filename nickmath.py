0bb86e6799f47c9f5c7d7476891432f37b9b0973
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2018-08-16 11:53:14 +0200

    added nickmath.py

---
+++ nickmath.py
@@ -0,0 +1,43 @@
+
+def add(x, y=2):
+    "Returns x + y, or x + 2."
+    result = x + y
+    return result
+
+
+def subtract(x, y=2):
+    result = x - y
+    return result
+
+
+def multiply(x, y):
+    """
+    Returns x * y
+
+    Arguments:
+        - x (int): the first number to multiply
+        - y (int): the second number to multiply
+
+    Returns:
+        - z (int): the number x * y
+
+    Examples:
+        >>> multiply(4, 5)
+        20
+
+        >>> multiply(5, 0)
+        0
+
+        >>> multiply(5, -4)
+        -20
+    """
+    y_is_negative = y < 0
+    z = sum([x for _ in range(abs(y))])
+    if y_is_negative:
+        z *= -1
+    return z
+
+# print(add(3, y=20) == 23)
+# print(multiply(3, 4) == 12)
+# print(multiply(4, 0) == 0)
+# print(multiply(4, -2) == -8)
